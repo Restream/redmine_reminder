773827c025b0d1440a8642915ab2eaa68280ee79
Danil Tashkinov <danil.tashkinov@gmail.com>
2012-09-10 19:18:43 +0400

    small fix in readme

--- init.rb
+++ init.rb
@@ -5,6 +5,7 @@ Redmine::Plugin.register :redmine_reminder do
   author 'Milan Stastny of ALVILA SYSTEMS'
   description 'E-mail notification of issues due date you are involved in (Assignee, Author, Watcher)'
   version '0.0.2'
+  url 'https://github.com/Undev/redmine_cut_tag'
   author_url 'http://www.alvila.com'
 end
 
