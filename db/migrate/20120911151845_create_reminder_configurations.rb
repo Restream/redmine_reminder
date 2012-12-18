bd70002a3b73de87742e51a3c0353352693c3b63
Danil Tashkinov <danil.tashkinov@gmail.com>
2012-12-18 20:03:08 +0400

    added checks to migrations

--- db/migrate/20120911151845_create_reminder_configurations.rb
+++ db/migrate/20120911151845_create_reminder_configurations.rb
@@ -1,6 +1,6 @@
 class CreateReminderConfigurations < ActiveRecord::Migration
   def self.up
-    create_table :reminder_configurations do |t|
+    create_table :reminder_configurations, :force => true do |t|
       t.column :days, :integer
       t.column :issue_status_selector, :string
       t.column :project_selector, :string
@@ -10,10 +10,10 @@ class CreateReminderConfigurations < ActiveRecord::Migration
       t.column :send_to_custom_user, :boolean
 
       t.timestamps
-    end
+    end unless table_exists? :reminder_configurations
   end
 
   def self.down
-    drop_table :reminder_configurations
+    drop_table :reminder_configurations if table_exists? :reminder_configurations
   end
 end
--- db/migrate/20120918093621_create_reminder_trackers.rb
+++ db/migrate/20120918093621_create_reminder_trackers.rb
@@ -3,10 +3,10 @@ class CreateReminderTrackers < ActiveRecord::Migration
     create_table :reminder_trackers do |t|
       t.column :reminder_configuration_id, :integer
       t.column :tracker_id, :integer
-    end
+    end unless table_exists? :reminder_trackers
   end
 
   def self.down
-    drop_table :reminder_trackers
+    drop_table :reminder_trackers if table_exists? :reminder_trackers
   end
 end
