22ffa7fdf74a3c976eb493bc8edb6112785e4b63
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-10-09 18:33:09 +0400

    fixed tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
 
-ActiveSupport::TestCase.append_fixture_path File.expand_path('../fixtures', __FILE__)
-
 class ActiveSupport::TestCase
   def mail_body(mail)
     mail.multipart? ? mail.parts.first.body.encoded : mail.body.encoded
